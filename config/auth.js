345b87ac2f9a0038278b8d396f9a9bfd1584eddb
Adrienne Tran <adrienne@adriennetran.com>
2017-08-14 16:09:34 -0400

    fixed redirect url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   facebookAuth: {
     clientID: '1461876320739853',
     clientSecret: '58d6766647ff8bb1e9ee85bd3dfd00bd',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: '/auth/facebook/callback',
   },
 };
